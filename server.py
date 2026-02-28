619fad46128ec5f469895ebb4d8ac9f4b55ca238
chandanmaurya7536-arch <chandanmaurya7536@gmail.com>
2026-03-01 00:48:33 +0530

    Refactor server.py to use environment variables
    Updated token and chat ID retrieval to use environment variables. Added a home route for server status.

--- server.py
+++ server.py
@@ -1,16 +1,21 @@
 from flask import Flask, request
 import requests
-
-TOKEN = "8777436576:AAEYiKjDdLROJUJoQeW3Ltf5iShIwG3VwRk"
-CHAT_ID = "8391954882"
+import os
 
 app = Flask(__name__)
+
+TOKEN = os.environ.get("8777436576:AAEYiKjDdLROJUJoQeW3Ltf5iShIwG3VwRk")
+CHAT_ID = os.environ.get("8391954882")
+
+@app.route("/")
+def home():
+    return "Server Running"
 
 @app.route("/upload", methods=["POST"])
 def upload():
     file = request.files["file"]
 
-    if file.filename.endswith((".jpg", ".png", ".jpeg")):
+    if file.filename.lower().endswith((".jpg", ".jpeg", ".png")):
         requests.post(
             f"https://api.telegram.org/bot{TOKEN}/sendPhoto",
             data={"chat_id": CHAT_ID},
@@ -25,4 +30,5 @@ def upload():
 
     return "Uploaded"
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
